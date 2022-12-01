09cc96b243e3611ca2ba126da2af0f3ae197b96a
Akari Katsuma <katainu@gmail.com>
2022-12-01 22:02:34 +0900

    Add URL used before

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from .views import TopView
+from .views import TopView, GameView
 
 urlpatterns = [
     path('', TopView.as_view(), name='top'),
+    path('about/', TopView.as_view()),
+    path('contact/', TopView.as_view()),
+    path('information/<pk>/', TopView.as_view()),
+    path('games/', GameView.as_view(), name='games'),
+    path('games/<pk>/', GameView.as_view()),
+    path('login/', TopView.as_view()),
 ]
 urlpatterns += staticfiles_urlpatterns()
