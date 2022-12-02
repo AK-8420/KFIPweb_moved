f5cce3800d7ad80b5a474a2245e208abd77cee10
Akari Katsuma <katainu@gmail.com>
2022-12-02 23:50:39 +0900

    change getElements method from tag name to id

--- staticfiles/js/redirect.js
+++ staticfiles/js/redirect.js
@@ -4,7 +4,7 @@ function timer(){
     $("#count").text(wait.toString());
     if(wait == 0){
         console.log("redirect");
-        location.href = $('a').attr('href');
+        location.href = $('#link').attr('href');
     }else{
         wait -= 1;
     }
